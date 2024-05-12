c964d8f9bee0f3f73575b7586edd5a3e8f69ea14
Md Shanto Hossain <mdshanto.seoexpate@gmail.com>
2024-05-12 16:09:59 -0700

    e-commerce phpto edit

--- press-release.php
+++ press-release.php
@@ -3,11 +3,206 @@ $title = "Home";
 ob_start();
 ?>
 
-<div class="py-5">
-	<div class="container">
-		<h4>Data Not Found</h4>
-	</div>
-</div>
+
+    <!--================================photo editor sub Header section end here=======================-->
+<?php include  'includes/sub-nav.php' ?>
+    <!--================================photo editor sub Header section end here=======================-->
+
+    <!--================================top-body section start here=======================-->
+    <section class="top-body app_development" style="background-image: url('frontend/images/1692873456499.jpg'); background-repeat: no-repeat;  background-position: center center; background-size: cover; padding-top: 20px;">
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-7 ecomresp" style="z-index:500; ">
+                    <div class="top_body_txt_part">
+                        <h1 class="marginbottom" style="font-size: 45px; line-height: 55px;">Best Press Release Writing Services in Bangladesh</h1>
+                        <p style=" margin-top: -15px;">What do you think is the best way to announce something about your product and service in a short period? Well, the answer is issuing a press release. You can easily transmit any announcement about your business using press releases. When you introduce a new product in the market, enter into a new market, or when you offer the sale of your products, press releases can be a speedy way to let your customers know about it. A press release is a news item for businesses to let their customers know what new products or services they are offering. It is a fantastic way to gain higher visibility of products and services of any business.</p>
+                    </div>
+                    <div class="top-body-button pt-3">
+                        <a type="button" href="contact.php">Contact Us</a>
+                    </div>
+                </div>
+                <div class="col-md-5 m-auto ecomrespp" style="z-index:500;" >
+                    <img src="frontend/images/Easy Scalability.webp" alt=" Press Release Writing Services" description=" Press Release Writing Services" class="img-thumbnail">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================================top-body section end here=======================-->
+
+    <!--================================cliping_service section start here=======================-->
+    <section class="cliping_service eCommerceSEO ecomrespp" style="background: var(--global-bg-color); padding: 15px 0;">
+        <div class="container">
+            <div class="row">
+                <div class="col-12" style="z-index:500;">
+                    <div class="top_about_us_txt" style="z-index:500;">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb breadcumb_gph">
+                                <li class="breadcrumb-item "><a href="index.php" class="text-light ">Home</a></li>
+                                <li class="breadcrumb-item "><a href="content_writing.php" class="text-light">Content Writing</a></li>
+                                <li class="breadcrumb-item active text-light" aria-current="page">Press Release Writing</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================================cliping_service section end here=======================-->
+
+    <!--================================editor_texts section start here=======================-->
+    <section class=" graphicdesigns_content">
+        <div class="container shadow px-5 py-5 border-top border-5 border-success">
+            <div class="row ">
+                <div class="col-12 ">
+                    <p>All the small and big businesses know the impact of a well-crafted press release. It will promote your business for an indefinite time if you can manage it properly. All the entrepreneurs nowadays use a press release to advertise their upcoming products or services. If you are offering products for a niche market, a press release can be your way to address the right customer base. Press releases are written and distributed on PR websites that many customers and businesses follow.</p>
+                </div>
+                <div class="col-12">
+                    <p>At <a href="index.php" class="text-success">SEO Expate</a> Bangladesh Ltd., we offer press release writing services for businesses across different industries. We have several in-house creative writers, and some of them are based on a journalism background. They can write a durable and attractive press release for your business. They have industry-specific knowledge, and they are very hard-working bright people. They can work closely with clients and make them satisfied. So, if you are looking for press release writers for hire, <a href="index.php" class="text-success">SEO Expate</a> Bangladesh Ltd. is your best shot. WE are the best because we are result-oriented, client-friendly and we have an outstanding track record for serving customers with satisfaction.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================================editor_texts section end here=======================-->
+
+    <!--================================editor_texts section start here=======================-->
+    <section class=" graphicdesigns_content" style=" background: #f7f7f7;">
+        <div class="container shadow py-5 px-5 " style="border-left:15px solid #F4B400; background: #fff;">
+            <div class="row ">
+                <div class="col-12">
+                    <h6 class="fw-bold fs-5 pb-3">Key Benefits of Press Release</h6>
+                    <div><p class="">The ultimate goal of blog writing is to reach a product or service to potential customers. Still, it is tough for a business to maintain blogs about their product, and to solve this problem, blog writing services come handy Here are some of the benefits blog writing services provide to businesses.</p></div>
+                    <h6 class="fw-bold fs-6 pb-3">Broadcast the News</h6>
+                    <div><p class="">When an industry expert writes about your product in a blog, he/she can explain the usability of your product and compare your product and let the readers know about the key features who’s made your product unique. By doing this, it can create a unique selling point for your product, which is necessary to compete in the competitive market.</p></div>
+                    <h6 class="fw-bold fs-6 pb-3">Brand Awareness</h6>
+                    <div><p class="">A quality blog writing services includes SEO optimized writing which helps a product to gain more web traffic. It also helps the product reach to the potential customer.</p></div>
+                    <h6 class="fw-bold fs-6 pb-3">Higher Media Coverage</h6>
+                    <div><p>Images make a blog look crisp and exciting. Pictures also help the reader to concentrate more on the article, which increases the potential sale. Product-related keep people interested in the product. It also helps the customer to understand what they are going to buy and how the product works.</p></div>
+                    <h6 class="fw-bold fs-6 pb-3">Acceptability</h6>
+                    <div><p>Research is the name of the game. A well-researched blog increases the credibility and reliability of a product. Well researched blog provides data that helps the customer to compare the products and fact check about the product before purchasing. Plagiarism-Free: the plagiarized article is like stealing, which represents a brand of the product very poorly. As an entrepreneur, it is your best interest to seek for 100% plagiarism-free report from your blog writing service.</p></div>
+                    <h6 class="fw-bold fs-6 pb-3">Building Reputation</h6>
+                    <div><p>Brand awareness is essential for your business. It defines your business and gives you loyal customers. To increase your brand awareness, you have to use blogs. With your creative content, you can send your message to your audiences and let them know what your business is offering and why they should take your product or service. This is how you create brand loyalty. But blogging is an ongoing process. It will take time, but the impact will be everlasting.</p></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================================editor_texts section end here=======================-->
+
+    <!--================================editor_texts section start here=======================-->
+    <section class=" graphicdesigns_content">
+        <div class="container shadow py-5 px-5 " style="border-left:15px solid var(--secondary-text-color); background: var(--bg-secondary-color);">
+            <div class="row ">
+                <div class="col-12">
+                    <h6 class="fw-bold fs-5 pb-3">Why SEO Expate Bangladesh Ltd  is best for Press Release Services</h6>
+                    <div><p>SEO Expate Bangladesh Ltd provides the best blog writing services in Bangladesh. We are very professional at our work. We know from experience how to fulfill requirements and make customers happy. We have expert team members who are very sincere about their work. They love what they do. Expertise and enhanced customer service are two of our competitive advantages.We know that as an entrepreneur, you are working hard to grow your business. We value your time, money, and effort. Let’s see some of the reasons why you should make us your partner in this beautiful journey:</p></div>
+                    <div><p>1. Webtech ensures that that specific industry expert writes the article you want. As a result, your blog articles will get profound depth, and it will appeal more to the customer.</p></div>
+                    <div><p>2. All team members are highly skilled and adequately experienced. They are writing blogs in various industries for a long time. They know everything about the industry they work on.</p></div>
+                    <div><p>3. Before publishing any article in the blog Webtech research about the topic and post a well researched legit article in the blog</p></div>
+                    <div><p>4. Webtech provides SEO optimized writing, which will increase the traffic to your item and will give more exposure to the potential customer.</p></div>
+                    <div><p>5. At Webtech, we are equipped with all the latest software tools and other latest technology necessary to serve you effectively and fast.</p></div>
+                    <div><p>6. We are also a guest posting service provider. We offer guest post writing services at Webtech.</p></div>
+                    <div><p>7. We provide a custom strategy for each of our clients because we know that a copy-paste strategy approach may not work for your business.</p></div>
+                    <div><p>8. We have a dedicated project manager for each of our clients.</p></div>
+                    <div><p>9. We have 5+ years of experience, and we have a satisfied customer base from the USA, UK, Canada, and Bangladesh.</p></div>
+                    <div><p>10. We have a 24/7 open and dedicated customer care service. You can always reach out to us without any hesitation.</p></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================================editor_texts section end here=======================-->
+
+    <!--================================accordion section start here=======================-->
+    <section class="accordion_service">
+        <div class="container">
+            <div class="row">
+                <h4 class="text-center pb-5 fs-2">Press Release Writing FAQ</h4>
+                <div id="accordion">
+                    <div class="accordion-item">
+                        <h5 class="accordion-header" id="headingOne" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Q. Why do I need a press release?</h5>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordion">
+                            <div class="accordion-body">At Webtech, we have clients from different industries. We have experts who are adequately experienced to work in various sectors. E-commerce, Healthcare, Education, IT, etc. are some of the many industries that we have covered so far. Our creative writers are bright, and they have industry-specific knowledge. Some of them are good at multiple trades.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h5 class="accordion-header collapsed" id="headingTwo" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Q. What factors do I have to consider before I go for a press release?
+                        </h5>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordion">
+                            <div class="accordion-body">Webtech is a digital agency that provides professional blog writing services with many other services. To take our service you have to place an order first. To place an order, you have to fill up the required forms carefully and submit it.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h5 class="accordion-header collapsed" id="headingThree" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">Q. Can I use pictures with my press release?
+                        </h5>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordion">
+                            <div class="accordion-body">Yes, we provide content rewrite services at Webtech. The marketplace today is very competitive, and competition is very high. It is essential to have new blogs and content on your website. Keeping this in mind, we offer content rewrite services at Webtech. It attracts customers and search engines. As a result, your website ranking will be higher, and you will have more leads at the same time.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h5 class="accordion-header collapsed" id="headingFour" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">Q. How your press release will stand out from others?
+                        </h5>
+                        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordion">
+                            <div class="accordion-body">Yes, we will post and manage blogs on your website. Our article and bog writing packages include managing and posting blogs on your website. Once we finish up writing blogs for your website, our editorial team will post them directly on your WordPress blog page.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h5 class="accordion-header collapsed" id="headingFourr" data-bs-toggle="collapse" data-bs-target="#collapseFourr" aria-expanded="false" aria-controls="collapseFourr">Q. What are the industries Webtech works into?
+                        </h5>
+                        <div id="collapseFourr" class="accordion-collapse collapse" aria-labelledby="headingFourr" data-bs-parent="#accordion">
+                            <div class="accordion-body">You must track your results after you apply content marketing services. You have to assess different matrix for different effects. For example, if you want to check if your brand awareness has increased, you have to track your website’s traffic, social share, bounce rate, etc.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item">
+                        <h5 class="accordion-header collapsed" id="headingFive" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">Q. Can I talk over the phone if I need to?
+                        </h5>
+                        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordion">
+                            <div class="accordion-body">Yes, we maintain quality, as this is one of our competitive advantages. We have separate quality control teams to ensure the quality of our services and to provide you with the best quality.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================================accordion section end here=======================-->
+
+    <!--================================top_ready_start section start here=======================-->
+    <section class="top_ready_start">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="top_ready_start_txt">
+                        <h3>We can build your story</h3>
+                        <h6>We have partnered with great companies and entrepreneurs all over the world. And, provided the best service for them</h6>
+                    </div>
+                    <div class="top-body-button pt-5">
+                        <a type="button" href="contact.php">Get In Touch</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="top_ready_start_img text-left">
+                        <img src="frontend/images/95991_prev_ui.png" alt=" Press Release Writing Services" description=" Press Release Writing Services">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================================top_ready_start section end here=======================-->
+
+    <!--================================Thrive_Globally section start here=======================-->
+    <section class="PAY_MENT" style="padding: 20px 0;">
+        <div class="container">
+            <div class="row">
+                <div class="PAY_IMG">
+                    <img src="frontend/images/Payment-method-banner-image-1024x73.webp" alt=" Press Release Writing Services" description=" Press Release Writing Services" width="100%">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================================Thrive_Globally section end here=======================-->
 
 <?php
 $main_content = ob_get_clean();
