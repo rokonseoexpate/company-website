fb91f2342405e0be1f17627a9f5b8565f86ce7de
rokonseoexpate <engrokon.rok@gmail.com>
2024-05-13 13:30:12 +0600

    page link up progress

--- b2b-seo.php
+++ b2b-seo.php
@@ -3,13 +3,218 @@
     ob_start();
 ?>
 
-<section class="top_our_transform">
-    <div class="container">
-        <div class="row">
-            <h1>No data found </h1>
+
+	<!--================================photo editor sub Header section end here=======================-->
+	<div class="photo_edipth">
+		<div class="container">
+			<nav class="navbar navbar-expand-lg navbar-dark ">
+				<div class="container-fluid ">
+				    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation">
+				        <span class="navbar-toggler-icon"><i class="fa-solid fa-bars" alt=" seo expate" description=" seo expate"></i></span>
+				    </button>
+				    <div class="collapse navbar-collapse  justify-content-center" id="main_nav">
+				        <ul class="navbar-nav ">
+				        	<li class="nav-item dropdown">
+				                <a class="nav-link  dropdown-toggle" href="seo_service.html" data-bs-toggle="dropdown">SEO Services</a>
+				                <ul class="dropdown-menu dropdown-menu-end">
+				                  <li><a class="dropdown-item" href="ecommerce_seo.html"> eCommerce SEO</a></li>
+				                  <li><a class="dropdown-item" href="amazon_seo.html">Amazon SEO</a></li>
+				                  <li><a class="dropdown-item" href="b2b_seo.html">B2B SEO</a></li>
+				                  <li><a class="dropdown-item" href="shopify_seo.html">Shopify SEO</a></li>
+				                  <li><a class="dropdown-item" href="wordpress_seo.html">WordPress SEO</a></li>
+				                  <li><a class="dropdown-item" href="woocommerce_seo.html">WooCommerce SEO</a></li>
+				                  <li><a class="dropdown-item" href="lead_seo.html">Lead Generation SEO</a></li>
+				                  <li><a class="dropdown-item" href="nopcommerce_seo.html">NopCommerce SEO</a></li>
+				                  <li><a class="dropdown-item" href="magento_seo.html">Magento SEO</a></li>
+				                  <li><a class="dropdown-item" href="volusion_seo.html">Volusion SEO</a></li>
+				                  <li><a class="dropdown-item" href="bigcommerce_seo.html">BigCommerce SEO</a></li>
+				                  <li><a class="dropdown-item" href="consulting_seo.html">SEO Consulting</a></li>
+				                </ul>
+				            </li>
+				            <li class="nav-item dropdown">
+				                <a class="nav-link  dropdown-toggle" href="ppc_paid.html" data-bs-toggle="dropdown">PPC & Paid Search</a>
+				                <ul class="dropdown-menu dropdown-menu-end">
+				                  <li><a class="dropdown-item" href="amazon_ppc.html"> Amazon PPC Management</a></li>
+				                  <li><a class="dropdown-item" href="ecommerce_ppc.html">eCommerce PPC Management</a></li>
+				                  <li><a class="dropdown-item" href="google_ads.html">Google Ads Consulting</a></li>
+				                  <li><a class="dropdown-item" href="management_ads.html">Google Ads Management</a></li>
+				                  <li><a class="dropdown-item" href="shoping_management.html">Google Shopping Management</a></li>
+				                  <li><a class="dropdown-item" href="ppc_consulting.html">PPC Consulting</a></li>
+				                  <li><a class="dropdown-item" href="ppc_lead.html">Lead Gen PPC Management</a></li>
+				                </ul>
+				            </li>
+				            <li class="nav-item"><a class="nav-link" href="onpage_seo.html"> On-page SEO </a></li>
+				            <li class="nav-item"><a class="nav-link" href="offpage_seo.html"> Off-page SEO </a></li>
+				            <li class="nav-item"><a class="nav-link" href="technical_seo.html"> Technical SEO </a></li>
+				            <li class="nav-item"><a class="nav-link" href="package_price_seo.html"> SEO Packages & Pricing </a></li>
+				        </ul>
+				    </div>
+				</div> 
+			</nav>      
+		</div>
+	</div>
+	<!--================================photo editor sub Header section end here=======================-->
+	
+	<!--================================top-body section start here=======================-->
+	<section class="top-body app_development" style="background-image: url(images/1692873456499.jpg); background-repeat: no-repeat;  background-position: center center; background-size: cover; padding-top: 20px;">		
+		<div class="container ">
+		    <div class="row">
+		    	<div class="col-md-7 ecomresp" style="z-index:500;">
+		    		<div class="top_body_txt_part">
+						<h1 class="marginbottom" style="font-size: 45px; line-height: 55px;">B2B SEO Services &  </h1>	
+						<h2 style="margin-top: -15px;" class="fs-1">Business-To-Business Search Marketing Agency</h2>	
+						<p style="text-align:justify;">As a B2B operation, it’s important to work with an SEO agency that understands B2B search engine optimization and its inherent challenges. To thrive online, B2B companies must develop a customized B2B SEO strategy based on their target audience, profit margins, lead time, business objectives and many other variables.</p>					
+					</div>
+					<div class="row">
+						<div class="col-md-6 pt-2">
+							<div class=" border-bottom border-3 border-success pb-3"><a href="contact.html" style="color: #F4B400;" class="fs-5 fw-bold ">Get Your B2B SEO Proposal</a></div>
+						</div>
+						<div class="col-md-2 ecomrespons">
+							<img src="frontend/images/google-seo-proposal-audit.png.webp" alt=" B2B SEO Services" description=" B2B SEO Services" class="img-fluid" >
+						</div>
+					</div>	
+				</div>
+				<div class="col-md-5 m-auto ecomrespp" style="z-index:500;" >
+					<img src="frontend/images/Adobe-Stock-201973585.jpg" alt=" B2B SEO Services" description=" B2B SEO Services" class="img-thumbnail">
+				</div>			
+			</div>
+		</div>				
+	</section>
+	<!--================================top-body section end here=======================-->
+
+	<!--================================cliping_service section start here=======================-->
+	<section class="cliping_service eCommerceSEO" style="background: var(--button-bg-hover-color); padding: 15px 0;">
+		<div class="container">
+			<div class="row">
+				<div class="col-12" style="z-index:500;">
+					<div class="top_about_us_txt" style="z-index:500;">
+			    		<nav aria-label="breadcrumb">
+						  <ol class="breadcrumb breadcumb_gph">
+						    <li class="breadcrumb-item "><a href="index.html" class="text-light ">Home</a></li>
+						    <li class="breadcrumb-item "><a href="seo_service.html" class="text-light">SEO</a></li>
+						    <li class="breadcrumb-item active text-light" aria-current="page">B2B SEO</li>
+						  </ol>
+						</nav>
+					</div>				
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================================cliping_service section end here=======================-->
+
+	<!--================================cliping_service section start here=======================-->
+	<section class="cliping_service" style="background: unset;">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6" style="z-index:500;">
+					<div class="cliping_service_txt pb-2"><h6>Advantages of Working with A Reputable B2B SEO <a href="index.html" class="text-success">Company</a></h6></div>
+					<p class="pt-3" >Obviously, an expert B2B SEO team at your disposal is a huge benefit. But it’s even better when that team is in-house, never outsourced, and ideally not located in a foreign country. We’re located in the USA and our team members work in our office on a daily basis. We’ll be your partner in success, knowing your success is ours.</p>												
+					<p class="" >When you’re selling to other businesses the rules of your website are often different. Many times pricing structures are prorated at the customer level, or certain sections of your website may require a customer to be logged in to view the content. It’s important to work with an SEO company that understands these challenges and has experience in operating successful B2B search engine marketing campaigns. We’ve worked with retailers selling over 75,00 unique SKU’s, down to major manufacturers who sell only a few items to large retailers. No matter your situation, talk over your search marketing needs with our team and we’ll see if it’s a fit for us to work together.</p>												
+				</div>
+				<div class="col-md-6" style="margin: auto;">
+					<div class="clip_img">
+						<img src="frontend/images/B2B-SEO-for-Growth-Marketers-1.png" alt=" Advantages of Working with A Reputable B2B SEO" description=" Advantages of Working with A Reputable B2B SEO" class="img-thumbnail">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--================================cliping_service section end here=======================-->
+
+	<!--================================editor_texts section start here=======================-->
+	<section class=" graphicdesigns_content" style=" background: var(--bg-secondary-color);">
+        <div class="container shadow py-5 px-5 " style="border-left:15px solid #F4B400;">
+            <div class="row ">
+            	<div class="col-12">
+                	<h6 class="fw-bold fs-4 pb-3">What B2B SEO services should the best agency provide?</h6>
+	                <div><p>Proper B2B SEO requires a full set of digital marketing services which is why our B2B SEO services include: </p></div>
+	                <h6 class="fw-bold fs-5 pb-3">1. B2B focused keyword list development</h6>
+	                <h6 class="fw-bold fs-5 pb-3">2. Demographic research & Competitor analysis</h6>
+	                <h6 class="fw-bold fs-5 pb-3">3. Technical SEO analysis</h6>
+	                <h6 class="fw-bold fs-5 pb-3">4. Landing page creation</h6>
+	                <h6 class="fw-bold fs-5 pb-3">5. Tag optimization</h6>
+	                <h6 class="fw-bold fs-5 pb-3">6. Content writing, creation, and strategy</h6>
+	                <h6 class="fw-bold fs-5 pb-3">7. Conversion optimization</h6>
+	                <h6 class="fw-bold fs-5 pb-3">8. Monthly meetings & reviews</h6>
+	                <h6 class="fw-bold fs-5 pb-3">9. Detailed SEO reporting</h6>
+                </div>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
+	<!--================================editor_texts section end here=======================-->
+
+	<!--================================cliping_service section start here=======================-->
+	<section class="cliping_service" style="background: unset; ">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6" style="margin: auto;">
+					<div class="clip_img">
+						<img src="frontend/images/1651138904258.png" alt=" Collaborate with B2B eCommerce Experts" description=" Collaborate with B2B eCommerce Experts" class="img-thumbnail">
+					</div>
+				</div>
+				<div class="col-md-6" style="z-index:500;">
+					<div class="cliping_service_txt pb-2 margintop"><h6>Collaborate with B2B eCommerce Experts</h6></div>
+					<p class="pt-3" >We don’t just market B2B websites, we build B2B eCommerce websites every single day. We understand every aspect of what it takes for a B2B website to become an effective channel for inbound leads and sales. With B2B experts in every area from project management, design, front and back end development,  <a href="seo_service.html" class="text-success">SEO</a>, & PPC management, operating under one roof, OuterBox delivers unrivaled value, expertise, and results.</p>																								
+				</div>				
+			</div>
+		</div>
+	</section>
+	<!--================================cliping_service section end here=======================-->
+
+	<!--================================editor_texts section start here=======================-->
+	<section class=" graphicdesigns_content" style="background: var(--bg-secondary-color);">
+        <div class="container shadow py-5 px-5 " style="border-left:15px solid var(--secondary-text-color);">
+            <div class="row ">
+            	<div class="col-12">
+                	<h6 class="fw-bold fs-4 pb-3">B2B Search Marketing Experience</h6>
+                	<p>OuterBox has experience with every type of B2B operation imaginable:</p>
+                	<h6 class="fw-bold fs-5 pb-3">1. Vertical B2B models </h6>
+	                <h6 class="fw-bold fs-5 pb-3">2. Horizontal B2B models </h6>
+	                <h6 class="fw-bold fs-5 pb-3">3. Suppliers</h6>
+	                <h6 class="fw-bold fs-5 pb-3">4. Distributors</h6>
+	                <h6 class="fw-bold fs-5 pb-3">5. Wholesalers</h6>
+	                <h6 class="fw-bold fs-5 pb-3">6. Sourcing, Procurement, Supply Chain & Logistics</h6>
+	                <h6 class="fw-bold fs-5 pb-3">7. Manufacturers</h6>
+                </div>
+            </div>
+        </div>
+    </section>
+	<!--================================editor_texts section end here=======================-->
+
+	<!--================================top_ready_start section start here=======================-->
+	<section class="top_ready_start">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="top_ready_start_txt">
+                        <h3>We can build your story</h3>
+                        <h6>We have partnered with great companies and entrepreneurs all over the world. And, provided the best service for them</h6>
+                    </div>
+                    <div class="top-body-button pt-5">
+						<a type="button" href="contact.html">Get In Touch</a>
+					</div>
+                </div>
+                <div class="col-md-6">
+                    <div class="top_ready_start_img text-left">
+                        <img src="frontend/images/95991_prev_ui.png" alt=" We can build your story" description=" We can build your story">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> 
+	<!--================================top_ready_start section end here=======================-->
+
+	<!--================================Thrive_Globally section start here=======================-->
+	<section class="PAY_MENT" style="padding: 20px 0;">
+        <div class="container">
+            <div class="row">
+                <div class="PAY_IMG">
+                 	<img src="frontend/images/Payment-method-banner-image-1024x73.webp" alt="payment" description="payment" width="100%">
+                </div>
+            </div>
+        </div>
+    </section>
+	<!--================================Thrive_Globally section end here=======================-->
 
 <?php 
 $main_content = ob_get_clean();
