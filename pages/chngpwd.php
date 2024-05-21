7bea8f9be0c03a16ffe3ff864bbc8369f49a09b0
Durjay Ghosh <durjay.seoexpate@gmail.com>
2024-05-21 09:44:33 +0600

    update

--- pages/chngpwd.php
+++ pages/chngpwd.php
@@ -2,11 +2,15 @@
 session_start();
 $file_path = '../config/dbconnect.php';
 
+
 if (file_exists($file_path)) {
     require_once($file_path);
 } else {
     echo "Error: The file '$file_path' does not exist.";
 }
+
+$db = new DB_con();
+$conn = $db->get_connection();
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -15,16 +19,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $newpassword = mysqli_real_escape_string($conn, $_POST['npwd']);
     $username = $_SESSION['username'];
 
-    $sanitized_username =  mysqli_real_escape_string($conn, $username); 
-    $sanitized_password =  mysqli_real_escape_string($conn, $oldpass); 
-        
+    $sanitized_username =  mysqli_real_escape_string($conn, $username);
+    $sanitized_password =  mysqli_real_escape_string($conn, $oldpass);
+
     $sql = "SELECT * FROM users WHERE username = '"
         . $sanitized_username . "' AND password = '"
-        . md5($sanitized_password) . "'"; 
-        
-    $result = mysqli_query($conn, $sql) 
-        or die(mysqli_error($conn)); 
-        
+        . md5($sanitized_password) . "'";
+
+    $result = mysqli_query($conn, $sql)
+    or die(mysqli_error($conn));
+
     $num = mysqli_fetch_array($result);
 
 
@@ -38,23 +42,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
             if ($update_result) {
                 $_SESSION['msg1'] = "Password Changed Successfully";
-                header('Location: changePassword');
+                header('Location: changePassword.php');
                 exit();
             } else {
                 $_SESSION['msg1'] = "Failed to update password";
-                header('Location: changePassword');
+                header('Location: changePassword.php');
                 exit();
             }
         } else {
             $_SESSION['msg1'] = "Invalid username or password";
-            header('Location: changePassword');
+            header('Location: changePassword.php');
             exit();
         }
     } else {
         $_SESSION['msg1'] = "Error: " . mysqli_error($conn);
-        header('Location: changePassword');
+        header('Location: changePassword.php');
         exit();
     }
-} 
+}
 
 ?>
