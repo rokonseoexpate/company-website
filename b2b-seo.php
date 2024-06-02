be690badac5d0b73781c62b1cbcaf57996bd1c7d
sm shamim <shamim.seoexpate@gmail.com>
2024-06-02 14:51:33 +0600

    head section add a-f

--- b2b-seo.php
+++ b2b-seo.php
@@ -1,7 +1,38 @@
 <?php
 $title = "B2B Seo";
-ob_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="Title" content="Seo Expate Bangladesh LTD.">
+	<meta name="description" content="Seo Expate Bangladesh LTD.">
+	<meta name="keywords" content="seoebl, seoexpate, Seo Expate Bangladesh LTD.">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $title;  ?></title>
+	<meta property="og:title" content="Home - IT Services, Technology Solutions">
+
+	<?php include('./includes/style.php') ?>
+
+	<div class="gtranslate_wrapper"></div>
+	<script>
+		window.gtranslateSettings = {
+			"default_language": "en",
+			"native_language_names": true,
+			"detect_browser_language": true,
+			"url_structure": "sub_domain",
+			"languages": ["en", "fr", "de", "it", "es"],
+			"wrapper_selector": ".gtranslate_wrapper",
+			"horizontal_position": "left",
+			"vertical_position": "bottom"
+		}
+	</script>
+	<script src="https://cdn.gtranslate.net/widgets/latest/lc.js" defer></script>
+
+</head>
+
 
 <?php include "includes/seo-nav.php" ?>
 
--- certificate.php
+++ certificate.php
@@ -1,11 +1,40 @@
 <?php
 $title = "Certificates";
-ob_start();
 require_once 'config/dbconnect.php';
 $db = new DB_con();
 $conn = $db->get_connection();
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="Title" content="Seo Expate Bangladesh LTD.">
+	<meta name="description" content="Seo Expate Bangladesh LTD.">
+	<meta name="keywords" content="seoebl, seoexpate, Seo Expate Bangladesh LTD.">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php echo $title;  ?></title>
+	<meta property="og:title" content="Home - IT Services, Technology Solutions">
+
+	<?php include('./includes/style.php') ?>
+
+	<div class="gtranslate_wrapper"></div>
+	<script>
+		window.gtranslateSettings = {
+			"default_language": "en",
+			"native_language_names": true,
+			"detect_browser_language": true,
+			"url_structure": "sub_domain",
+			"languages": ["en", "fr", "de", "it", "es"],
+			"wrapper_selector": ".gtranslate_wrapper",
+			"horizontal_position": "left",
+			"vertical_position": "bottom"
+		}
+	</script>
+	<script src="https://cdn.gtranslate.net/widgets/latest/lc.js" defer></script>
+
+</head>
 
 <!--================================top-body section start here=======================-->
 <section class="top-body top_about_us" style="background-image: url(frontend/images/awards-and-recognitions-background-bannerr.png); background-repeat: no-repeat;  background-position:center center; background-size: cover; padding-top: 180px !important; padding-bottom: 300px;">
@@ -47,7 +76,7 @@ $conn = $db->get_connection();
 								<h4><?php echo $row['title'] ?></h4>
 								<p><?php echo $row['description'] ?></p>
 							</div>
-						
+
 						</div>
 						<div class="col-md-6">
 							<div class="Core_Values_img pb-5 pt-3 text-center">
